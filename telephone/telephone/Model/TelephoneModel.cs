cd8a72c2621284caee266a8b081f89a5576d5fd4
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-09-24 21:50:21 -0300

    Criação telefone model

---
+++ telephone/telephone/Model/TelephoneModel.cs
@@ -0,0 +1,10 @@
+﻿namespace telephone.Model
+{
+    public class TelephoneModel
+    {
+        public int Id { get; set; }
+        public int Ddd { get; set; }
+        public int Number { get; set; }
+        public bool Ative { get; set; }
+    }
+}
